20a5c4f4dc2c4bb3283b5eaef345696b685885df
nillmishra <nillmishra09@gmail.com>
2025-08-14 18:12:04 +0530

    updated database

--- server/src/db.js
+++ server/src/db.js
@@ -4,13 +4,15 @@ import mongoose from 'mongoose';
 export async function connectMongo() {
   const uri = process.env.MONGODB_URI;
   if (!uri) {
-    console.error('MONGODB_URI not set in .env');
+    console.error('MONGODB_URI not set in env');
     process.exit(1);
   }
+  const opts = {};
+  if (process.env.DB_NAME) opts.dbName = process.env.DB_NAME; // optional DB override
+
+  mongoose.set('strictQuery', true);
   try {
-    await mongoose.connect(uri, {
-      // options can be added here
-    });
+    await mongoose.connect(uri, opts);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
